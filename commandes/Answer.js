8838299b58624e6cc6d70c97b561907ebf7518f3
HANSTZ <hanstzmd@gmail.com>
2025-01-15 01:27:47 +0300

    Update Answer.js

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -8,68 +8,107 @@ const s = require(__dirname + "/../set");
 const more = String.fromCharCode(8206)
 const readmore = more.repeat(4001)
 
-zokou({ nomCom: "hanspayment", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
-    let { cm } = require(__dirname + "/../framework//zokou");
-    var coms = {};
-    var mode = "public";
-    
-    if ((s.MODE).toLocaleLowerCase() != "yes") {
-        mode = "private";
-    }
-
-
-    
-
-    cm.map(async (com, index) => {
-        if (!coms[com.categorie])
-            coms[com.categorie] = [];
-        coms[com.categorie].push(com.nomCom);
-    });
-
-    moment.tz.setDefault('Etc/GMT');
-
-// Créer une date et une heure en GMT
-const temps = moment().format('HH:mm:ss');
-const date = moment().format('DD/MM/YYYY');
-
-let infoMsg =  `
-1. **Recipient Name:** Mariam Billy Jumanne  
-2. **Mobile Number:** 0756530143 (Vodacom) 
-3. **Hello 👋\n${nomAuteurMessage}
-5. **Payment Method:** Online Payment  
-6. **Country:** Tanzania 🇹🇿
-`;
-    
-let menuMsg = `
-  `;
-
-   var lien = mybotpic();
-
-   if (lien.match(/\.(mp4|gif)$/i)) {
+zokou({ nomCom: "hanstech", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *hansmd*, déveloper hans Tech" , gifPlayback : true }, { quoted: ms });
+      if (!arg || arg.length === 0) {
+        return repondre(`Please ask a question.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const question = arg.join(' ');
+      const response = await axios.get(`https://api.davidcyriltech.my.id/ai/chatbot?query=${question}`);
+      
+      const data = response.data;
+      if (data) {
+        repondre(data.result);
+      } else {
+        repondre("Error during response generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request.");
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
-    }
-} 
-// Vérification pour .jpeg ou .png
-else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+  });
+
+
+  zokou({ nomCom: "dalle", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *hansmd*, déveloper hans Tech" }, { quoted: ms });
+      if (!arg || arg.length === 0) {
+        return repondre(`Please enter the necessary information to generate the image.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const image = arg.join(' ');
+      const response = await axios.get(`https://api.davidcyriltech.my.id/ai/gpt4omini?text=${image}`);
+      
+      const data = response.data;
+      let caption = '*powered by HANS-MD*';
+      
+      if (data.status == 200) {
+        // Utiliser les données retournées par le service
+        const imageUrl = data.result;
+        zk.sendMessage(dest, { image: { url: imageUrl }, caption: caption }, { quoted: ms });
+      } else {
+        repondre("Error during image generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request");
     }
-    catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
-        repondre("🥵🥵 Menu erreur " + e);
+  });
+  
+  zokou({ nomCom: "ai", reaction: "📡", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please ask a question.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const question = arg.join(' ');
+      const response = await axios.get(`https://api.davidcyriltech.my.id/ai/chatbot?query=${question}`);
+      
+      const data = response.data;
+      if (data) {
+        repondre(data.result);
+      } else {
+        repondre("Error during response generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request.");
     }
-} 
-else {
-    
-    repondre(infoMsg + menuMsg);
-    
-}
+  });
 
-}); 
 
+zokou({ nomCom: "gpt", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+    const { repondre, arg, ms } = commandeOptions;
+  
+    try {
+      if (!arg || arg.length === 0) {
+        return repondre(`Please ask a question.`);
+      }
+  
+      // Regrouper les arguments en une seule chaîne séparée par "-"
+      const question = arg.join(' ');
+      const response = await axios.get(`https://api.davidcyriltech.my.id/ai/chatbot?query=${question}`);
+      
+      const data = response.data;
+      if (data) {
+        repondre(data.result);
+      } else {
+        repondre("Error during response generation.");
+      }
+    } catch (error) {
+      console.error('Erreur:', error.message || 'Une erreur s\'est produite');
+      repondre("Oops, an error occurred while processing your request.");
+    }
+  });
+
+
+  
